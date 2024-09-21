3405687af7fa344eada16f576c447cee93453e2b
Kyrylko <kkuzmenko1992@gmail.com>
2024-09-21 10:33:42 -0230

    generics

---
+++ src/generics/4.ts
@@ -0,0 +1,13 @@
+type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
+function saveUserDetails(partialData: Partial<User>) {}
+
+saveUserDetails({
+  email: "user@mail.com",
+  password: "password123",
+});
---
+++ src/generics/6.ts
@@ -0,0 +1,17 @@
+
+type ValidationErrors = {
+  email?: string[];
+  firstName?: string[];
+  lastName?: string[];
+  phone?: string[];
+};
+
+type UserForm = {
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  phone: string | null;
+  validationErrors: ValidationErrors;
+};
+
+type FormParams = Omit<UserForm, "validationErrors">;
